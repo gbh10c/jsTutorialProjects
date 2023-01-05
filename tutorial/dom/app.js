a48fdadc78b5cccc98be730cd6e4fceede8ec6f4
Gordon Hicken <gordon.hicken@gmail.com>
2023-01-05 16:54:36 -0500

    getElementById example

---
+++ tutorial/dom/app.js
@@ -0,0 +1,14 @@
+// getElementById
+
+// const h1 = document.getElementById('title');
+// h1.style.color = 'red';
+
+// document.getElementById('btn').style.backgroundColor = 'blue';
+// document.getElementById('btn').style.color = 'white';
+
+// const btn = document.getElementById('btn');
+
+// btn.style.backgroundColor = 'blue';
+// btn.style.color = 'white';
+
+
