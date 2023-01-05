3ed085c0bb9bb29440c4c090ecca5dd5d717dc3a
Gordon Hicken <gordon.hicken@gmail.com>
2023-01-05 10:30:52 -0500

    Updated folder name

---
+++ tutorial/arrayIterators/script.js
@@ -0,0 +1,83 @@
+const people = [
+  { name: 'bob', age: 20, position: 'developer', id: 1, salary: 200 },
+  { name: 'peter', age: 25, position: 'designer', id: 2, salary: 300 },
+  { name: 'susy', age: 30, position: 'assistant boss', id: 3, salary: 400 },
+  { name: 'anna', age: 35, position: 'boss', id: 4, salary: 500 },
+];
+
+// forEach
+// does not return new array
+
+// people.forEach((person) => console.log(person.position.toUpperCase()));
+
+// map
+// returns new array
+// does not change size of original Array
+// uses values from original array when making new one
+
+// set to variable because it returns a new array
+// const ages = people.map(function (person) {
+//   return person.age + 20;
+// });
+
+// const newPeople = people.map((person) => {
+//   return {
+//     firstName: person.name.toUpperCase(),
+//     oldAge: person.age + 20,
+//   };
+// });
+
+// const names = people.map((person) => {
+//   return `<h1>${person.name}</ h1>`;
+// });
+
+// document.body.innerHTML = names.join('');
+
+// filter
+// returns a new array
+// CAN manipulate the size of new array
+// returns based on condition
+
+// const youngPeople = people.filter((person) => {
+//   return person.age < 30;
+// });
+
+// const developers = people.filter((person) => {
+//   return person.position === 'developer';
+// });
+
+// console.log(developers);
+
+// find
+// returns a single instance (an object, here)
+// returns first match, if no match undefined
+// great for getting unique value
+
+// const person = people.find((person) => {
+//   return person.id === 3;
+// });
+
+// console.log(person);
+// console.log(person.name);
+
+// // find returns a string in this instance
+// const names = ['bob', 'peter', 'susy', 'anna'];
+
+// const firstName = names.find((name) => {
+//   return name === 'anna';
+// });
+
+// console.log(firstName);
+
+// reduce
+// iterates, callback function
+// reduces to a single value - number, array, object (list this after callback)
+// 1st parameter ('acc') - total of all calculations
+// 2nd parameter ('curr') - current iteration/value
+// ALWAYS RETURN ACCUMULATOR
+
+const total = people.reduce((acc, curr) => {
+  return acc + curr.salary;
+}, 0);
+
+console.log(total)
