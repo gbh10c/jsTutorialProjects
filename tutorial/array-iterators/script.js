502238c60e219d0fb893bf49d340be04c04deb7b
Gordon Hicken <gordon.hicken@gmail.com>
2023-01-04 22:39:03 -0500

    Added map and filter

--- tutorial/array-iterators/script.js
+++ tutorial/array-iterators/script.js
@@ -1,10 +1,49 @@
-// FOR EACH
-// does not return new array
-
 const people = [
   { name: 'bob', age: 20, position: 'developer' },
   { name: 'peter', age: 25, position: 'designer' },
-  { name: 'susy', age: 30, position: 'boss' },
+  { name: 'susy', age: 30, position: 'assistant boss' },
+  { name: 'anna', age: 35, position: 'boss' },
 ];
 
-people.forEach((person) => console.log(person.position.toUpperCase()));
+// forEach
+// does not return new array
+
+// people.forEach((person) => console.log(person.position.toUpperCase()));
+
+// map
+// returns new array
+// does not change size of original Array
+// uses values from original array when making new one
+
+// set to variable because it returns a new array
+// const ages = people.map(function (person) {
+//   return person.age + 20;
+// });
+
+// const newPeople = people.map((person) => {
+//   return {
+//     firstName: person.name.toUpperCase(),
+//     oldAge: person.age + 20,
+//   };
+// });
+
+// const names = people.map((person) => {
+//   return `<h1>${person.name}</ h1>`;
+// });
+
+// document.body.innerHTML = names.join('');
+
+// filter
+// returns a new array
+// CAN manipulate the size of new array
+// returns based on condition
+
+const youngPeople = people.filter((person) => {
+  return person.age < 30;
+});
+
+const developers = people.filter((person) => {
+  return person.position === 'developer';
+});
+
+console.log(developers);
