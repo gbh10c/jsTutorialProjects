20effb51e63a3c786f5d7642cb7ae5f59e0037e7
Gordon Hicken <gordon.hicken@gmail.com>
2023-01-05 10:39:42 -0500

    Added data.js file and moved array from script to data

---
+++ tutorial/challengesArrayMethods/data.js
@@ -0,0 +1,7 @@
+const students = [
+  { id: 1, name: 'peter', score: 80, favoriteSubject: 'math' },
+  { id: 2, name: 'coda', score: 90, favoriteSubject: 'music' },
+  { id: 3, name: 'jason', score: 95, favoriteSubject: 'math' },
+  { id: 4, name: 'eddy', score: 80, favoriteSubject: 'art' },
+  { id: 5, name: 'jacob', score: 95, favoriteSubject: 'math' },
+];
