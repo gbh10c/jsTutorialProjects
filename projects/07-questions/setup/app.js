bdfd6e9b560a6a4e7611109b6ef0322fbe66e463
Gordon Hicken <gordon.hicken@gmail.com>
2023-01-08 22:39:53 -0500

    Completed JS by traversing the DOM and also by using selectors inside the element

--- projects/07-questions/setup/app.js
+++ projects/07-questions/setup/app.js
@@ -1,2 +1,23 @@
-//using selectors inside the element
 // traversing the dom
+// const questionBtns = document.querySelectorAll('.question-btn');
+
+// questionBtns.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
+
+//using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((selection) => {
+  const btn = selection.querySelector('.question-btn');
+  btn.addEventListener('click', () => {
+    questions.forEach((question) => {
+      if (question !== selection) question.classList.remove('show-text');
+    });
+
+    selection.classList.toggle('show-text');
+  });
+});
