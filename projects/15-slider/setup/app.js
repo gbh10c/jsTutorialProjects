64a8ba1b4f6dc31f71d89fa8f4e7339f90e89080
Gordon Hicken <gordon.hicken@gmail.com>
2023-01-12 10:55:13 -0500

    Added JS for carousel with beginning and end along with hidden prev and next buttons

--- projects/15-slider/setup/app.js
+++ projects/15-slider/setup/app.js
@@ -19,12 +19,28 @@ nextBtn.addEventListener('click', () => {
 
 function carousel() {
   //working with continuous slides
-  if (counter === slides.length) {
-    counter = 0;
-  } else if (counter < 0) {
-    counter = slides.length - 1;
+  // if (counter === slides.length) {
+  //   counter = 0;
+  // } else if (counter < 0) {
+  //   counter = slides.length - 1;
+  // }
+
+  //working with beginning and end of list show/hide buttons
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = 'block';
+  } else {
+    nextBtn.style.display = 'none';
   }
+
+  if (counter > 0) {
+    prevBtn.style.display = 'block';
+  } else {
+    prevBtn.style.display = 'none';
+  }
+
   slides.forEach((slide) => {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 }
+
+prevBtn.style.display = 'none';
