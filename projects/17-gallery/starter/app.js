d99f702662662a78175c2c580db425d4dafd0d6d
Gordon Hicken <gordon.hicken@gmail.com>
2023-01-18 10:40:03 -0500

    Bound 'this' to open modal on click event

--- projects/17-gallery/starter/app.js
+++ projects/17-gallery/starter/app.js
@@ -9,6 +9,7 @@ function getElement(selection) {
 }
 
 function Gallery(element) {
+  this.container = element;
   this.list = [...element.querySelectorAll('.img')];
   // target
   this.modal = getElement('.modal');
@@ -17,7 +18,22 @@ function Gallery(element) {
   this.closeBtn = getElement('.close-btn');
   this.nextBtn = getElement('.next-btn');
   this.prevBtn = getElement('.prev-btn');
+  // bind functions
+  // this.openModal = this.openModal.bind(this);
+  // container event
+  this.container.addEventListener(
+    'click',
+    function (e) {
+      this.openModal();
+    }.bind(this)
+  );
 }
+
+Gallery.prototype.openModal = function () {
+  console.log(this);
+  console.log('open modal');
+  this.modal.classList.add('open');
+};
 
 const nature = new Gallery(getElement('.nature'));
 const city = new Gallery(getElement('.city'));
