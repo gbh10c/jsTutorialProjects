1853cc084797582e659b0e02f3769c294508f55e
Gordon Hicken <gordon.hicken@gmail.com>
2023-06-09 13:02:00 -0400

    Completed Simple Text XMLHttpRequest

--- tutorial/ajaxTutorial/app.js
+++ tutorial/ajaxTutorial/app.js
@@ -2,7 +2,7 @@ const xhr = new XMLHttpRequest();
 
 xhr.open('GET', './api/sample.txt');
 xhr.onreadystatechange = function () {
-  // console.log(xhr);
+  console.log(xhr);
   if (xhr.readyState === 4 && xhr.status === 200) {
     const text = document.createElement('p');
     text.textContent = xhr.responseText;
@@ -11,10 +11,7 @@ xhr.onreadystatechange = function () {
     console.log({
       status: xhr.status,
       text: xhr.statusText,
-      state: xhr.readyState,
     });
   }
 };
 xhr.send();
-
-console.log('hello world');
