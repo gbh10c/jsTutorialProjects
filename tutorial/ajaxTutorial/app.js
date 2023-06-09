e3b0d5f875029e6ab6fce0975b78dee46e5b571c
Gordon Hicken <gordon.hicken@gmail.com>
2023-06-09 13:45:00 -0400

    Set up first fetch

--- tutorial/ajaxTutorial/app.js
+++ tutorial/ajaxTutorial/app.js
@@ -1,1 +1,22 @@
+const url = './api/people.json';
 
+// const response = fetch(url);
+// console.log(response);
+
+// fetch(url)
+//   .then((resp) => {
+//     // response object
+
+//     // convert response into JSON data
+//     // returns promise
+//     return resp.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.log(err));
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
