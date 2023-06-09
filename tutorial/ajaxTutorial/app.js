c2eb9d3b1e2995b31af6bb873c5392a2c34ccfcc
Gordon Hicken <gordon.hicken@gmail.com>
2023-06-09 13:58:37 -0400

    Displayed JSON data with fetch api

--- tutorial/ajaxTutorial/app.js
+++ tutorial/ajaxTutorial/app.js
@@ -1,22 +1,22 @@
 const url = './api/people.json';
 
-// const response = fetch(url);
-// console.log(response);
+const btn = document.querySelector('.btn');
 
-// fetch(url)
-//   .then((resp) => {
-//     // response object
+btn.addEventListener('click', () => {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => displayItems(data))
+    .catch((err) => console.log(err));
+});
 
-//     // convert response into JSON data
-//     // returns promise
-//     return resp.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((err) => console.log(err));
-
-fetch(url)
-  .then((resp) => resp.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+const displayItems = (items) => {
+  const displayData = items
+    .map((item) => {
+      const { name } = item;
+      return `<p>${name}</p>`;
+    })
+    .join('');
+  const element = document.createElement('div');
+  element.innerHTML = displayData;
+  document.body.appendChild(element);
+};
