8617883a316b78c3e00f9229235633b7f38f35bb
Gordon Hicken <gordon.hicken@gmail.com>
2023-06-11 15:31:54 -0400

    Added fetch functionality, initial joke fetching, and loading text

--- projects/21-dad-jokes/starter/app.js
+++ projects/21-dad-jokes/starter/app.js
@@ -3,7 +3,19 @@ const btn = document.querySelector('.btn');
 const result = document.querySelector('.result');
 
 btn.addEventListener('click', () => {
-  console.log('button clicked!');
+  fetchDadJoke();
 });
 
-// function fetchJoke() 
+const fetchDadJoke = async () => {
+  result.textContent = 'Loading...'
+  const response = await fetch(url, {
+    headers: {
+      Accept: 'application/json',
+      'User-Agent': 'learning app',
+    },
+  });
+  const data = await response.json();
+  result.textContent = data.joke;
+};
+
+fetchDadJoke();
