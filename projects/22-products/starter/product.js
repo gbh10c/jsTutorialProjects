5a9b3436a55de27a8d73209d1367862039ed4760
Gordon Hicken <gordon.hicken@gmail.com>
2023-06-30 13:07:01 -0400

    Set up functionality to fetch product id

--- projects/22-products/starter/product.js
+++ projects/22-products/starter/product.js
@@ -1,18 +1,28 @@
 const productDOM = document.querySelector('.product');
-const url =
-  'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog';
+const url = 'https://course-api.com/javascript-store-single-product';
 
-  const fetchProduct = async () => {
-    return 'product'
-  }
+const fetchProduct = async () => {
+  try {
+    productDOM.innerHTML = '<h4 class="product-loading">Loading...</h4>';
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
 
-  const displayProduct = (product) => {
-    console.log(product);
+    const response = await fetch(`${url}?id=${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    productDOM.innerHTML =
+      '<p class="error">There was a problem loading the product. Please try again later.</p>';
   }
+};
+
+const displayProduct = (product) => {
+  console.log(product);
+};
 
 const start = async () => {
   const data = await fetchProduct();
   displayProduct(data);
-}
+};
 
 start();
