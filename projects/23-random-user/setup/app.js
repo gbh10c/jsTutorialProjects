421d4d3ac4d21da7197927221a3bbb1dcdce0a01
Gordon Hicken <gordon.hicken@gmail.com>
2023-06-30 15:03:46 -0400

    Completed logic for button assignments and active status

--- projects/23-random-user/setup/app.js
+++ projects/23-random-user/setup/app.js
@@ -1,10 +1,17 @@
 import get from './utils/getElement.js';
+import getUser from './utils/fetchUser.js';
+import displayUser from './utils/displayUser.js';
 
-const URL = 'https://randomuser.me/api/';
 
-const img = get('.user-img');
-const title = get('.user-title');
-const value = get('.user-value');
 const btn = get('.btn');
-const btns = [...document.querySelectorAll('.icon')];
-console.log(btns);
+
+
+const showUser = async () => {
+  //get user from api
+  const person = await getUser();
+  //display user
+  displayUser(person);
+};
+
+window.addEventListener('DOMContentLoaded', showUser);
+btn.addEventListener('click', showUser);
