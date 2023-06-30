2b8f04ae56e5a4fc2b563ef1363e35febfcde61b
Gordon Hicken <gordon.hicken@gmail.com>
2023-06-30 13:24:45 -0400

    Set up dynamic values to display single products and color variations

--- projects/22-products/starter/product.js
+++ projects/22-products/starter/product.js
@@ -17,7 +17,36 @@ const fetchProduct = async () => {
 };
 
 const displayProduct = (product) => {
-  console.log(product);
+  const {
+    company,
+    colors,
+    description,
+    name: title,
+    price,
+    image,
+  } = product.fields;
+  const { url: img } = image[0];
+  document.title = title.toUpperCase();
+
+  // colors
+  const colorsList = colors
+    .map((color) => {
+      return `<span class="product-color" style="background:${color}"></span>`;
+    })
+    .join('');
+  productDOM.innerHTML = `<div class="product-wrapper">
+        <img src="${img}" alt="${title}" class="img" />
+        <div class="product-info">
+          <h3>${title}</h3>
+          <h5>${company}</h5>
+          <span>${price / 100}</span>
+          <div class="colors">${colorsList}</div>
+          <p>
+            ${description}
+          </p>
+          <button class="btn">Add to Cart</button>
+        </div>
+      </div>`;
 };
 
 const start = async () => {
